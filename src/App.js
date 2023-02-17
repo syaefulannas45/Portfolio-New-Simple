60433429f566cd221b5bd2cc1c15f42d4dac790b
syaefulannas45 <syaefulannas33@gmail.com>
2023-02-17 16:05:42 +0700

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 function App() {
+  useEffect(() => {
+    document.title = "Syaeful Annas | Web Dev"
+  })
   return (
     <>
       <Navbar />
