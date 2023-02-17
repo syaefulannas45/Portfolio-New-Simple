2671b943dcf46a4b95a18602b062061575bc20a3
syaefulannas45 <syaefulannas33@gmail.com>
2023-02-17 19:33:34 +0700

    portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
         <section id='footer'>
-            <div className='bg-pastel-700 py-10'>
+            <div className='bg-pastel-700 py-10 text-sm'>
                 <p className='text-center text-slate-800'>All Right Reserved. Syaeful Annas</p>
             </div>
         </section>
