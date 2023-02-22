44feaa10c05f92c0a9fc7df66764090e993d5865
Syaeful Annas <100054932+syaefulannas45@users.noreply.github.com>
2023-02-22 17:38:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
 
 function App() {
   return (
     <>
       <Navbar />
       <About />
-      <Project />
       <Footer/>
     </>
   );
